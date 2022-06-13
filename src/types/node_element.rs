20c76c3de10aa78d3300e17303bd2a92d08aa35f
heyyyoyy <heyyyoyy@gmail.com>
2022-06-13 21:51:58 +0300

    deserialize test for NodeElement

--- src/types/node_element.rs
+++ src/types/node_element.rs
@@ -13,9 +13,37 @@ enum NodeElementAttr {
     Src(String)
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Default)]
 pub struct NodeElement {
     tag: String,
     attrs: Option<HashMap<NodeElementAttr, String>>,
     children: Option<Vec<Node>>
+}
+
+
+#[cfg(test)]
+mod tests {
+    use serde_json;
+
+    use super::{NodeElement, Node};
+
+    #[test]
+    fn node_elements_deserialize() {
+        let page_views_str = r#"
+        {
+            "tag": "p",
+            "children": ["Hello world!"]
+        }"#;
+        let node_element: NodeElement = serde_json::from_str(page_views_str).unwrap_or_default();
+        assert_eq!(node_element.tag, "p");
+        let node = node_element.children.unwrap_or_default().into_iter().nth(0);
+
+        let el = if let Some(Node::String(el)) = node {
+            el
+        } else {
+            String::new()
+        };
+
+        assert_eq!(el, "Hello world!");    
+    }
 }

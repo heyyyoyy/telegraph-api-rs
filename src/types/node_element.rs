f270751cdebe8a6553a9e795d349882b23254171
heyyyoyy <heyyyoyy@gmail.com>
2022-06-14 21:58:25 +0300

    add test for NodeElement

--- src/types/node_element.rs
+++ src/types/node_element.rs
@@ -1,22 +1,18 @@
 use serde::Deserialize;
 
-use std::collections::HashMap;
-
 use super::node::Node;
 
 
-#[derive(Deserialize, PartialEq, Eq, Hash)]
-pub enum NodeElementAttr {
-    #[serde(alias = "href")]
-    Href(String),
-    #[serde(alias = "src")]
-    Src(String)
+#[derive(Deserialize, Default)]
+pub struct NodeElementAttr {
+    pub href: Option<String>,
+    pub src: Option<String>
 }
 
 #[derive(Deserialize, Default)]
 pub struct NodeElement {
     pub tag: String,
-    pub attrs: Option<HashMap<NodeElementAttr, String>>,
+    pub attrs: Option<NodeElementAttr>,
     pub children: Option<Vec<Node>>
 }
 
@@ -29,12 +25,12 @@ mod tests {
 
     #[test]
     fn node_elements_deserialize() {
-        let page_views_str = r#"
+        let node_el_str = r#"
         {
             "tag": "p",
             "children": ["Hello world!"]
         }"#;
-        let node_element: NodeElement = serde_json::from_str(page_views_str).unwrap_or_default();
+        let node_element: NodeElement = serde_json::from_str(node_el_str).unwrap_or_default();
         assert_eq!(node_element.tag, "p");
 
         let node = node_element.children.unwrap_or_default().into_iter().nth(0);
@@ -45,5 +41,17 @@ mod tests {
         };
 
         assert_eq!(el, "Hello world!");    
+    }
+
+    #[test]
+    fn node_elements_with_attr_deserialize() {
+        let node_el_str = r#"
+        {
+            "tag": "p",
+            "attrs": {"href": "link1"}
+        }"#;
+        let node_element: NodeElement = serde_json::from_str(node_el_str).unwrap_or_default();
+    
+        assert_eq!(node_element.attrs.unwrap_or_default().href.unwrap_or_default(), "link1"); 
     }
 }
